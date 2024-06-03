8a13240327d4b5115d54d394a0bedcfce263b132
Maciej Katafiasz <mathrick@gmail.com>
2024-06-03 06:57:52 -0700

    Better editor UI for NullCardCollection

--- addons/simple_card_pile_ui/card_db/null_card_collection.gd
+++ addons/simple_card_pile_ui/card_db/null_card_collection.gd
@@ -1,6 +1,11 @@
+@tool
 class_name NullCardCollection extends CardCollection
 
 @export var cards: Array
 
 func _get_all():
 	return cards
+
+func _validate_property(property):
+	if property.name == "all":
+		property.usage &= ~PROPERTY_USAGE_EDITOR
